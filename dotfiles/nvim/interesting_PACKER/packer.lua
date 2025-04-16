c3114a366893b82591376653dc12166187fa0c49
metahke <fioletowy@tutanota.com>
2025-04-16 18:22:09 +0200

    Files structure optimization

---
+++ dotfiles/nvim/interesting_PACKER/packer.lua
@@ -0,0 +1,13 @@
+-- load harpoon.nvim plugin
+use('theprimeagen/harpoon')
+
+-- load undotree.nvim plugin
+use('mbbill/undotree')
+
+-- load vim-fugitive.nvim plugin
+use('tpope/vim-fugitive')
+
+-- load lsp-zero.nvim plugin (UNMAINTAINED)
+use({'neovim/nvim-lspconfig'})
+use({'hrsh7th/nvim-cmp'})
+use({'hrsh7th/cmp-nvim-lsp'})
