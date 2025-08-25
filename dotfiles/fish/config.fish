02bfc574af06864d30b711f5755119f4df78c5a7
metahke <fioletowy@tutanota.com>
2025-08-25 13:45:36 +0200

    Update Fish Shell config

--- dotfiles/fish/config.fish
+++ dotfiles/fish/config.fish
@@ -1,7 +1,16 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 
-    # fzf configuration
+    # Add Cargo binaries to PATH
+    set PATH $PATH $HOME/.cargo/bin
+
+    # Change default Fish greeing prompt
+    set -U fish_greeting ""
+
+    # Set default fzf options
     set -gx FZF_DEFAULT_COMMAND "fd . $HOME"
     set -gx FZF_DEFAULT_OPTS "--exact"
+
+    # Set aliases
+    alias zola="flatpak run org.getzola.zola"
 end
